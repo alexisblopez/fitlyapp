db357422e1d8f7f67549555941c3c1f1e21f1344
alexisblopez <alxs.lpz@gmail.com>
2016-12-31 16:52:56 -0600

    sweater INSERT

--- php/sweater.php
+++ php/sweater.php
@@ -1,6 +1,21 @@
 <?php
 
-echo $_POST['sname'];
-echo $_POST['semail'];
+if (isset($_POST['submit'])) {
+
+$con = mysql_connect("localhost","root","ajdwgplp");
+
+if (!$con) {
+    die("Cannot connect: " . mysql_error());
+}
+
+mysql_select_db("fitly",$con);
+
+$sql = "INSERT INTO sweaters (name,email) VALUES ('$_POST[sname]','$_POST[semail]')";
+
+mysql_query($sql,$con);
+
+mysql_close($con);
+    
+}
 
 ?>
